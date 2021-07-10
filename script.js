8fcb64072ed2dc96c5ba1f06b1ed27cebda71ebe
Zoltan Bano <banozoltan@gmail.com>
2021-07-10 11:48:44 +0200

    store message elements

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Thank you for running Mixed Messages!')
+
+// storing topics and message elements
+const messageStart = 'Congratulations! Your topic is: '
+
+const topic = ['birds', 'music', 'code'];
+
+const messageMiddle = '!\nToday you should learn about: ';
+
+const birds = ['blackbird', 'swallow', 'blue jay', 'cuckoo', 'woodpecker', 'dove', 'sparrow'];
+const music = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian'];
+const code = ['JavaScript', 'Java', 'Python', 'C#', 'C++', 'PHP', 'HTML/CSS'];
+
+const messageEnd = '!'
+
+const motivation = ['Keep up the good work!', 'You can do it!', 'You are awesome!'];
+
+
+
