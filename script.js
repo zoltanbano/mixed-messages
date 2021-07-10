d39d513f6442458da67a8fcc43d701519f9296f8
Zoltan Bano <banozoltan@gmail.com>
2021-07-10 12:29:09 +0200

    specifications met

--- script.js
+++ script.js
@@ -11,9 +11,31 @@ const birds = ['blackbird', 'swallow', 'blue jay', 'cuckoo', 'woodpecker', 'dove
 const music = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian'];
 const code = ['JavaScript', 'Java', 'Python', 'C#', 'C++', 'PHP', 'HTML/CSS'];
 
-const messageEnd = '!'
+const elements = [birds, music, code];
+
+const messageEnd = '!\n'
 
 const motivation = ['Keep up the good work!', 'You can do it!', 'You are awesome!'];
+
+// functions to create the final message
+const getRandomElement = array => array[Math.floor(Math.random() * array.length)];
+
+const messageAssembler = () => {
+    let m = [];
+    m.push(messageStart);
+    let t = getRandomElement(topic);
+    m.push(t);
+    m.push(messageMiddle);
+    m.push(getRandomElement(elements[topic.indexOf(t)]));
+    m.push(messageEnd);
+    m.push(getRandomElement(motivation));
+    return m;
+}
+
+const message = messageAssembler().join('');
+
+console.log(message);
+
 
 
 
